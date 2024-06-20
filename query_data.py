af4d3e64a4f1e36b6934cda46e66e7f42d5f80ef
Aditya (Adi) Dhanotia <130966362+adityadhanotia@users.noreply.github.com>
2024-06-19 18:15:42 -0700

    Add files via upload

---
+++ query_data.py
@@ -0,0 +1,63 @@
+import argparse
+# from dataclasses import dataclass
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Set OpenAI API key
+openai_api_key = os.environ['OPENAI_API_KEY']
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Parse the query text from the command line.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+
+    # Load the vector DB that we created before
+    embedding_function = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7: #therefore here we access the float value from tuple
+        print(f"Unable to find matching results.")
+        return
+        #return type is List[Tuple[Document, float]
+
+    # Generate a prompt for the chat model.
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    # print(prompt) ##uncomment this to see the prompt we're giving to LLM
+    
+    # Use the chat model to generate a response.
+    model = ChatOpenAI(openai_api_key=openai_api_key)
+    response_text = model.predict(prompt)
+    
+    # Print the response and sources.
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+
+
+if __name__ == "__main__":
+    main()
